759f624dbf701af3506bd96a5d375de83240173f
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-03-28 11:32:17 -0700

    Added FrameManipulation files Moved createRGB to this file.
    Added prototypes for some functions we discussed

---
+++ GoofyLightsEditor/FrameManipulation.cpp
@@ -0,0 +1,49 @@
+
+#include "FrameManipulation.h"
+
+
+// Function creates a 2d memory element of the RGB struct then passes back the pointer to it.
+// source http://stackoverflow.com/questions/936687/how-do-i-declare-a-2d-array-in-c-using-new
+t_RGB** create_RGB(int r, int c)
+{
+    t_RGB** arr = new t_RGB*[r];
+    for(int i = 0; i < r; ++i)
+        arr[i] = new t_RGB[c];
+    return arr;
+}
+
+
+// creates a RGB Array and returns a pointer to it.
+t_RGB** create_RGB(int r, int c);
+
+
+// Takes origional frame and returnes a new copy of it
+//https://github.com/CS383Team2/Goofy_Lights_Editor/issues/9
+t_FrameData copyFrame(t_FrameData FrameData)
+{
+    //Black magic
+}
+
+// translates from by a given direction
+//https://github.com/CS383Team2/Goofy_Lights_Editor/issues/7
+int translateFrame(t_FrameData d, int direction)
+{
+    //Black magic
+}
+
+// Fills given frame with color
+//https://github.com/CS383Team2/Goofy_Lights_Editor/issues/6
+int fillFrame(t_FrameData d, t_RGB rgb)
+{
+    //Black magic
+}
+
+int fillFrame2(t_FrameData d, short r, short g, short b)
+{
+    t_RGB rgb;
+    rgb.R = r;
+    rgb.G = g;
+    rgb.B = b;
+
+    fillFrame(d, rgb);
+}
