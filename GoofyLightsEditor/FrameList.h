036ccaa61e9220be8b10f89e414f91a5ea18cd7a
Kevin Dorscher <kevindorscher@yahoo.com>
2017-03-30 14:49:41 -0700

    "AddNode" changed to "AddTail"
    Also added DeleteList function to delete all nodes in the linked list upon termination
    This function is also in FrameList.cpp

--- GoofyLightsEditor/FrameList.h
+++ GoofyLightsEditor/FrameList.h
@@ -52,15 +52,23 @@ public:
         }
     }                                                                                                                      
     // Add a node onto the end of the linked list.
-    void AddNode(t_FrameData x);
-    
+    void AddTail(t_FrameData x);
+	
+    // Function will call DeleteNode for every item in the Linked List, and delete
+    // the head node until all items in the linked list have been deleted.
+    void DeleteList();
+	
     // Delete the first node in the list.
     void DeleteNode();
 	
     // Add node at position x in the list
+    // If pos == 0 this refers to the head node, and
+    // If pos == count - 1 this refers to the tail node.
     void AddNode_Middle(t_FrameData x, int pos);
 	
     // Delete node at position x in the list
+    // If pos == 0 this refers to the head node, and
+    // If pos == count - 1 this refers to the tail node.
     void DeleteNode_Middle(int pos);
  
     // Return the first node found in the list
