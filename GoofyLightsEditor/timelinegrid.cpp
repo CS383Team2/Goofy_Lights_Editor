3c43a538d4c0fbc462935d65b97237d00d9cef4f
YupHio <yuphio@gmail.com>
2017-04-04 11:23:14 -0700

    TIMELINE is coming soon...

---
+++ GoofyLightsEditor/timelinegrid.cpp
@@ -0,0 +1,6 @@
+#include "timelinegrid.h"
+
+TimelineGrid::TimelineGrid()
+{
+
+}
---
+++ GoofyLightsEditor/timelinegrid.h
@@ -0,0 +1,11 @@
+#ifndef TIMELINEGRID_H
+#define TIMELINEGRID_H
+
+
+class TimelineGrid
+{
+public:
+    TimelineGrid();
+};
+
+#endif // TIMELINEGRID_H
