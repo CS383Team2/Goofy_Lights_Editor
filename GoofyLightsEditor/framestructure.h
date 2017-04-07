b12816c4db5acf16d72a0134529bbcee54b03911
gjcarter <cart8282@vandals.uidaho.edu>
2017-04-06 22:33:31 -0700

    Removed RGB structure as it's handled by Qt now. Renamed newdata to squaredata and fixed a typo with duration throughtou all code.

--- GoofyLightsEditor/framestructure.h
+++ GoofyLightsEditor/framestructure.h
@@ -3,23 +3,12 @@
 
 #include "gridsquare.h"
 
-// RGB
-typedef struct
-{
-    unsigned short R;    // Red
-    unsigned short G;    // Green
-    unsigned short B;    // Blue
-} t_RGB;
-
 // Frame data
 typedef struct
 {
     unsigned long ID;         // Unique ID of frame. Static once generated.
-    unsigned int durration;   // Frame Duration in MS
-    unsigned short r;   //Number of rows in frame
-    unsigned short c;   //Number of columns in frame
-    t_RGB **data;
-    GridSquare **newdata;  //Frame data
+    unsigned int duration;   // Frame Duration in MS
+    GridSquare **squareData;  //Frame data
 } t_FrameData;
 
 #endif // FRAMESTRUCTURE_H
