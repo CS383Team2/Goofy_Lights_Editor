e77ab013891f34a084895ed857bf9d194d7a8167
YupHio <yuphio@gmail.com>
2017-04-09 15:05:00 -0700

    You guys used the wrong object I think. -P

--- GoofyLightsEditor/framestructure.h
+++ GoofyLightsEditor/framestructure.h
@@ -1,14 +1,21 @@
 #ifndef FRAMESTRUCTURE_H
 #define FRAMESTRUCTURE_H
 
-#include "gridsquare.h"
+//#include "gridsquare.h" // WRONG -P
+#include <timelinegrid.h>
 
 // Frame data
 typedef struct
 {
     unsigned long ID;         // Unique ID of frame. Static once generated.
     unsigned int duration;   // Frame Duration in MS
-    GridSquare **squareData;  //Frame data
+
+    //GridSquare **squareData;  //Frame data    WRONG -P
+    //There will only ever be one GridSquare object, for the grid.
+    //Each FrameData will however have its own TimelineGrid object, since the timeline will have more than one item
+
+    TimelineGrid **squareData; //fixed -P
+
 } t_FrameData;
 
 #endif // FRAMESTRUCTURE_H
