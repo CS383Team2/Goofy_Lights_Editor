d58978aff832cc0507791394bc8b5dd8d4a6a9ed
gjcarter <cart8282@vandals.uidaho.edu>
2017-05-08 18:34:25 -0700

    Some more cleanup. Added some very basic info to about menu with link to our GitHub. Changed around sizing window a little bit to look a little more pretty.

--- GoofyLightsEditor/framestructure.h
+++ GoofyLightsEditor/framestructure.h
@@ -3,12 +3,13 @@
 
 #include <timelinegrid.h>
 
-// Frame data
+//Structre containing the data for each frame. This includes frame position,
+//duration, and the color for each square of the frame.
 typedef struct
 {
     int ID = 0;				// Unique ID of frame. Static once generated.
     int Position = 0;			// Each Frames position in the time line, starting with (Head Nodes Position = 0)
-    double duration = 0.20;	   			// Frame Duration in MS
+    double duration = 0.2;	   			// Frame Duration in MS
 
     TimelineGrid **squareData  = new TimelineGrid*[20]; //Always set to 20
 
